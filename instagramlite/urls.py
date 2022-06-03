257f41c49db3bf7d5aee12a9e5e4b1a4664ae577
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-06-03 21:01:42 +0300

    adding urls for authentication

--- instagramlite/urls.py
+++ instagramlite/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     url('admin/', admin.site.urls),
     url(r'', include('instagramu.urls')),
     url(r'^tinymce/', include('tinymce.urls')),
+    url(r'users/', include('users.urls')),
+    url(r'users/', include('django.contrib.auth.urls')),
 ]
 admin.site.site_header= "Instagram Lite Administration"
 admin.site.site_title="Instagram"
--- instagramu/views.py
+++ instagramu/views.py
@@ -1,5 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .models import *
+from .forms import *
+from django.contrib.auth import login, authenticate
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
 
 # Create your views here.
+# def homepage(request):
+#     return render(request, 'insta/homepage.html')
+
 def homepage(request):
-    return render(request, 'insta/homepage.html')
+    Images = Image.get_images()
+    comments = Comment.get_comment()
+    profile = Profile.get_profile()
+
+    current_user = request.user
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = current_user
+            comment.save()
+        return redirect('home')
+
+    else:
+        form = CommentForm()
+
+    return render(request,"insta/homepage.html",{"Images":Images, "comments":comments,"form": form,"profile":profile})
