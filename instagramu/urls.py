f106b0778a45c1374e014a65ecb517aa696a7ef9
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-06-04 11:49:32 +0300

    adding search template

--- instagramu/urls.py
+++ instagramu/urls.py
@@ -11,8 +11,8 @@ urlpatterns=[
     url('like/(?P<operation>.+)/(?P<pk>\d+)',views.like, name='like'),
     url('profile/(\d+)', views.user_profile, name='profile'),
     url('^new/profile', views.add_user_profile, name='add_profile'),
-    # url(r'^search/', views.search_results, name='search_results'),
- 
+    url('^search/', views.search_results, name='search_results'),
+  
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
