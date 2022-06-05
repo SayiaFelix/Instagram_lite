e4cfcab07e10fe366e9c23adeb7553746bb810a1
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-06-05 21:07:37 +0300

    Adding confirmation email

--- instagramlite/settings.py
+++ instagramlite/settings.py
@@ -116,7 +116,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'sayiafelix18@gmail.com'
-EMAIL_HOST_PASSWORD = 'rtryugjgkjk'
+EMAIL_HOST_PASSWORD = '0706206335'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
