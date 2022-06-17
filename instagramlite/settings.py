346412df311c181a4b3fc96d15cf5441439b7108
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-06-17 12:30:53 +0300

    Adding API Endpoints

--- instagramlite/settings.py
+++ instagramlite/settings.py
@@ -116,7 +116,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'fyugyua@gmail.com'
-EMAIL_HOST_PASSWORD = 'tmyiee'
+EMAIL_HOST_PASSWORD = 'te'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
